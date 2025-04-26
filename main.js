0b1d59b78a3d7d696d3f4f4466c286a46ba4d141
Erikaa5 <erika859317@gmail.com>
2025-04-26 18:00:23 -0300

    Agregar botón y funcionalidad de modo oscuro

---
+++ main.js
@@ -0,0 +1,12 @@
+const botonModo = document.getElementById("modoOscuroBtn");
+  const body = document.body;
+
+  botonModo.addEventListener("click", function () {
+    body.classList.toggle("modo-oscuro");
+
+    if (body.classList.contains("modo-oscuro")) {
+      botonModo.textContent = "Modo Claro";
+    } else {
+      botonModo.textContent = "Modo Oscuro";
+    }
+  });
